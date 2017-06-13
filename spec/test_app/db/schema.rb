eea27dce67f49fadec3b0848c3a47a6f58870654
Alexandre de Oliveira <chavedomundo@gmail.com>
2017-06-13 15:58:38 -0300

    Adiciona schema

--- spec/test_app/db/schema.rb
+++ spec/test_app/db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170219175007) do
+ActiveRecord::Schema.define(version: 20170613184809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
+
+  create_table "cas_categories", force: :cascade do |t|
+    t.uuid     "section_id"
+    t.string   "name"
+    t.string   "slug"
+    t.string   "metadata"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "cas_contents", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
     t.uuid     "section_id"
@@ -25,9 +34,28 @@ ActiveRecord::Schema.define(version: 20170219175007) do
     t.boolean  "published"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "summary"
+    t.integer  "pageviews"
+    t.string   "metadata"
     t.index ["author_id"], name: "index_cas_contents_on_author_id", using: :btree
     t.index ["published"], name: "index_cas_contents_on_published", using: :btree
     t.index ["section_id"], name: "index_cas_contents_on_section_id", using: :btree
+  end
+
+  create_table "cas_media_files", force: :cascade do |t|
+    t.integer  "attachable_id"
+    t.string   "attachable_type"
+    t.uuid     "author_id"
+    t.string   "service"
+    t.text     "title"
+    t.string   "url"
+    t.string   "mime_type"
+    t.string   "original_name"
+    t.integer  "size"
+    t.text     "file_data"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["attachable_type", "attachable_id"], name: "index_cas_media_files_on_attachable_type_and_attachable_id", using: :btree
   end
 
   create_table "cas_sections", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
@@ -36,6 +64,7 @@ ActiveRecord::Schema.define(version: 20170219175007) do
     t.uuid     "site_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "slug"
     t.index ["site_id"], name: "index_cas_sections_on_site_id", using: :btree
   end
 
@@ -44,6 +73,16 @@ ActiveRecord::Schema.define(version: 20170219175007) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_cas_sites_on_name", using: :btree
+  end
+
+  create_table "cas_users", force: :cascade do |t|
+    t.string   "name",                    null: false
+    t.string   "login",                   null: false
+    t.string   "password",                null: false
+    t.uuid     "author_id"
+    t.string   "roles",      default: [],              array: true
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
 end
