2bfc5115b8718ef808c9169507f79bcdbaf292d5
Luan Jodar <luan.jodar@gmail.com>
2017-06-23 16:34:52 -0300

    Add field category in new contents

--- app/models/cas/category.rb
+++ app/models/cas/category.rb
@@ -1,5 +1,6 @@
 module Cas
   class Category < ApplicationRecord
     belongs_to :section
+    has_many :contents
   end
 end
--- app/models/cas/section.rb
+++ app/models/cas/section.rb
@@ -2,5 +2,6 @@ module Cas
   class Section < ApplicationRecord
     belongs_to :site
     has_many :contents
+    has_many :categories
   end
 end
