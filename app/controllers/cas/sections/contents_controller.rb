fe663ed5624ffca89a1c829498292dec4f18e453
Luan Jodar <luan.jodar@gmail.com>
2017-06-29 23:13:39 -0300

    Add delete in content

--- app/controllers/cas/sections/contents_controller.rb
+++ app/controllers/cas/sections/contents_controller.rb
@@ -25,13 +25,11 @@ module Cas
     end
 
     def edit
-      @section = Section.find(params[:section_id])
       @content = ::Cas::Content.friendly.find(params[:id])
       @categories = @section.categories
     end
 
     def update
-      @section = Section.find(params[:section_id])
       @content = ::Cas::Content.friendly.find(params[:id])
 
       if @content.update(content_params)
@@ -40,7 +38,14 @@ module Cas
         render 'edit'
       end
     end
- 
+
+    def destroy
+      @content = ::Cas::Content.friendly.find(params[:id])
+      @content.destroy
+
+      redirect_to section_contents_path
+    end
+
     private
 
     def content_params
